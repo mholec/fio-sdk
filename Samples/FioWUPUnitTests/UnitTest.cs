9baf093ed724e260bbef41891513ac3ec4c51245
Arci <Arcidev@users.noreply.github.com>
2016-04-08 22:27:07 +0200

    Added support for Windows Universal Platform

---
+++ Samples/FioWUPUnitTests/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using FioSdkCsharp;
+using FioSdkCsharp.Models;
+using System.Diagnostics;
+
+namespace FioWUPUnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            ApiExplorer explorer = new ApiExplorer("YOUR_TOKEN_MUST_BE_PRESENT_HERE");
+
+            // get new transactions from last check
+            AccountStatement newTransactions = explorer.Last().Result;
+
+            // browse transactions
+            foreach (var transaction in newTransactions.TransactionList.Transactions)
+            {
+                Debug.WriteLine(transaction + " - " + transaction.Amount.Value);
+            }
+        }
+    }
+}
---
+++ Src/FioSdkCsharp.WUP/ApiExplorer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using FioSdkCsharp.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FioSdkCsharp
+{
+    public class ApiExplorer
+    {
+        private readonly string _authToken;
+
+        public ApiExplorer(string authToken)
+        {
+            _authToken = authToken;
+        }
+
+        /// <summary>
+        /// Returns transactions in selected period
+        /// </summary>
+        public async Task<AccountStatement> Periods(TransactionFilter filter)
+        {
+            string url = string.Format(FioUrl.Periods, _authToken, filter.DateFrom, filter.DateTo, Format.Json.ToString().ToLowerInvariant());
+
+            try
+            {
+                string data = await DownloadData(url);
+                return JsonConvert.DeserializeObject<RootObject>(data).AccountStatement;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Data can not be provided", e);
+            }
+        }
+
+        /// <summary>
+        /// Returns new transactions from last call
+        /// </summary>
+        public async Task<AccountStatement> Last()
+        {
+            string url = string.Format(FioUrl.Last, _authToken, Format.Json.ToString().ToLowerInvariant());
+
+            try
+            {
+                string data = await DownloadData(url);
+                return JsonConvert.DeserializeObject<RootObject>(data).AccountStatement;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Data can not be provided", e);
+            }
+        }
+
+        /// <summary>
+        /// Returns transactions in selected period in requested format
+        /// </summary>
+        public async Task<string> Periods(TransactionFilter filter, Format format)
+        {
+            string url = string.Format(FioUrl.Periods, _authToken, filter.DateFrom, filter.DateTo, format.ToString().ToLowerInvariant());
+
+            try
+            {
+                return await DownloadData(url);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Data can not be provided", e);
+            }
+        }
+
+        /// <summary>
+        /// Returns new transactions from last call in requested format
+        /// </summary>
+        public async Task<string> Last(Format format)
+        {
+            string url = string.Format(FioUrl.Last, _authToken, format.ToString().ToLowerInvariant());
+
+            try
+            {
+                return await DownloadData(url);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Data can not be provided", e);
+            }
+        }
+
+        /// <summary>
+        /// Changes last check date (suitable for Last() method)
+        /// </summary>
+        public async Task SetLastDownloadDate(DateTime date)
+        {
+            string url = string.Format(FioUrl.SetLast, _authToken, date.ToString(Constants.DateFormat));
+
+            try
+            {
+                await DownloadData(url);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Last download date has not been changed", e);
+            }
+        }
+
+        /// <summary>
+        /// Downloads data as string from FIO API 
+        /// </summary>
+        private async Task<string> DownloadData(string url)
+        {
+            using (var http = new HttpClient())
+            {
+                return await http.GetStringAsync(url);
+            }
+        }
+    }
+}
