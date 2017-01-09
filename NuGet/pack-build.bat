e198422235878dd3203c074a8dba8767b3afe262
Miroslav Holec <mirek.holec@hotmail.com>
2017-01-09 18:17:04 +0100

    Updated assembly files

--- NuGet/pack-build.bat
+++ NuGet/pack-build.bat
@@ -2,7 +2,7 @@ REM delete old packages
 rd package
 rd build
 
-REM create folders for target frameworks
+REM create folders for all target frameworks
 if not exist build mkdir build
 if not exist package\lib\net45 mkdir package\lib\net45
 if not exist package\lib\net451 mkdir package\lib\net451
@@ -16,3 +16,5 @@ copy %~dp0\..\Src\FioSdkCsharp.Net451\bin\Release\FioSdkCsharp.dll package\lib\n
 copy %~dp0\..\Src\FioSdkCsharp.Net452\bin\Release\FioSdkCsharp.dll package\lib\net452
 copy %~dp0\..\Src\FioSdkCsharp.Net46\bin\Release\FioSdkCsharp.dll package\lib\net46
 copy %~dp0\..\Src\FioSdkCsharp.Uap100\bin\Release\FioSdkCsharp.dll package\lib\uap10.0
+
+REM another task must create nupkg package during the build process
